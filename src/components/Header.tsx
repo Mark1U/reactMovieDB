cf3b8654bfc994baa8b8cbdd7703555b94a8461f
Mark1U <mark@zurcloud.com>
2024-12-19 11:18:30 +0100

    Init

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header>
+      <nav className="flex items-center justify-center px-8  py-4">
+        <h1 className="text-2xl font-bold">Image Gallery</h1>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
